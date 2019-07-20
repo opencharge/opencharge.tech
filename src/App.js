f2d699a2803175f4125785ea6d15956bf7e6e43f
Anatoly <rook11@list.ru>
2019-07-20 20:05:24 +0300

    card: add theme

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Layout from './Components/Layout/Layout';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App page theme theme_color_whitepaper-default theme_space_default theme_breakpoint_default theme_size_default theme_gap_medium theme_menu_default">
       <Layout />
     </div>
   );
