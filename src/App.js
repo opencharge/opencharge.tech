b46fd80495fa5a730f3be4e0c893a07faa4ba487
Ilya Kolebayev <kolebayev@gmail.com>
2019-07-23 11:52:51 +0300

    Add files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { withNaming } from '@bem-react/classname';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const cn = withNaming({ e: '__', m: '_' });
+const theme = cn('theme');
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className={theme({ 
+          color: 'whitepaper-default',
+          size: 'default',
+          space: 'default',
+          breakpoint: 'default',
+          control: 'whitepaper-default'
+      })}>
+        <div style={{ backgroundColor: 'var(--color-bg-default)' }} className="text text_size_l text_view_primary">
+          ывывыв
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
+
+
