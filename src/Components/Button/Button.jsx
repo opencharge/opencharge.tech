9ecdb11d1b62bec2fa430d9e99c99140757ca832
Tolik_designer <tolik_designer@hotmail.com>
2019-07-30 13:35:53 +0300

    update login

--- src/Components/Button/Button.jsx
+++ src/Components/Button/Button.jsx
@@ -8,7 +8,7 @@ const cnButton = cn('button');
 class Button extends Component {
     render() {
         return (
-            <button className={`${cnButton(null, [cnButton({ width: 'full', size: 'l', view: 'secondary' })])} ${this.props.buttonDecorator}`}>
+            <button className={`${cnButton(null, [cnButton({ width: 'full', size: 'l', view: 'secondary', form:'round' })])} ${this.props.buttonDecorator}`}>
                 {this.props.text}
             </button>
         );
--- src/Components/LoginForm/LoginForm.jsx
+++ src/Components/LoginForm/LoginForm.jsx
@@ -11,6 +11,7 @@ const cnPtForm = cn('pt-form');
 const cnPtCard = cn('pt-card');
 const cnDecorator = cn('decorator');
 const cnText = cn('text')
+const cnButton = cn('button');
 
 class LoginForm extends Component {
     render() {
@@ -20,25 +21,26 @@ class LoginForm extends Component {
 
                     <Logo />
 
-                    <div className={`${cnPtForm(null, [cnPtForm({ view: 'default', border: 'all'  })])} ${cnDecorator(null, [cnDecorator({'space-a': '3xl'})])}`}>
+                    <div className={`${cnPtForm(null, [cnPtForm({ view: 'default' })])} ${cnDecorator(null, [cnDecorator({'space-a': '3xl'})])}`}>
                         <div className={cnDecorator(null, [cnDecorator({'indent-b': '2xl', })])}>
                             <div className={cnText(null, [cnText({ weight: 'bold', size: '2xl', view: 'primary'})])}>
                                 Login
                             </div>
                         </div>
-
-                        <Input placeholder={this.props.loginPlaceholder} inputDecorator={cnDecorator(null, [cnDecorator({'indent-b': 'l'})])}/>
-                        <Input placeholder={this.props.passwordPlaceholder} inputDecorator={cnDecorator(null, [cnDecorator({'indent-b': 'l'})])}/>
-                        <Button text={this.props.buttonText} />
-                        
+                        <button className={`${cnButton(null, [cnButton({ width: 'full', size: 'l', view: 'primary', form:'round'  })])}`}>
+                            Login with Facebook
+                        </button>
                         <div className={cnDecorator(null, [cnDecorator({'indent-t': 'xl', 'indent-b': 'xl'})])}>
                             <div className={cnText(null, [cnText({ size: 'xl', view: 'primary'})])}>
                                 or
                             </div>
                         </div>
-
-                         <Social />
-
+                        <Input placeholder={this.props.loginPlaceholder} inputDecorator={cnDecorator(null, [cnDecorator({'indent-b': 'l'})])}/>
+                        <Input placeholder={this.props.passwordPlaceholder} inputDecorator={cnDecorator(null, [cnDecorator({'indent-b': 'l'})])}/>
+                        <Button text={this.props.buttonText} />
+                        <div className={cnDecorator(null, [cnDecorator({'indent-t': '2xl', })])}>
+                            <Social />  
+                        </div>
                     </div>
                 </div>
             </div>
