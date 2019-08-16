cee60f2ae0b0ad3dfdcc61460f7cd8d5a9b5bef4
Ilya Kolebayev <kolebayev@gmail.com>
2019-08-16 12:37:53 +0300

    Init

--- src/App.js
+++ src/App.js
@@ -3,26 +3,35 @@ import { withNaming } from '@bem-react/classname';
 
 const cn = withNaming({ e: '__', m: '_' });
 const theme = cn('theme');
+const layout = cn('layout');
 
 
 class App extends Component {
   render() {
     return (
-      <div className={theme({ 
-          color: 'whitepaper-default',
-          size: 'default',
-          space: 'default',
-          breakpoint: 'default',
-          control: 'whitepaper-default'
+      <div className={theme({
+        color: 'whitepaper-default',
+        size: 'default',
+        space: 'default',
+        breakpoint: 'default',
+        control: 'whitepaper-default'
       })}>
-        <div style={{ backgroundColor: 'var(--color-bg-default)' }} className="text text_size_l text_view_primary">
-          ывывыв
-        </div>
+
+          <div className={layout({ structure: '100' })} style={{ minHeight: '100vh' }}>
+            <div className={layout('section')}>
+              <div className={layout('content')}>
+                <div className={layout('container', { size: 'm', distribute: 'center' })}>
+                  <div classname={}>
+
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
       </div>
     );
   }
 }
 
 export default App;
-
-
